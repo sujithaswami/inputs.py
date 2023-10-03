a273baeec0048639a8cf000e8b07c1837d42221b
sujithaswami <144520487+sujithaswami@users.noreply.github.com>
2023-10-03 16:21:00 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,53 @@
+#arthmatic
+
+a = input("enter a value")
+b = input("enter b value")
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+#logical
+
+print(int(a) & int(b))
+print(int(a) | int(b))
+print(int(a) ^ int(b))
+
+#relational
+print(int(a) <= int(b))
+print(int(a) >= int(b))
+print(int(a) == int(b))
+print(int(a) != int(b))
+print(int(a) < int(b))
+print(int(a) > int(b))
+
+#Bitwise
+print(int(a) | int(b))
+print(int(a) ^ int(b))
+print(int(a) >> int(b))
+print(int(a) << int(b))
+
+#identity
+print(int(a) is int(b))
+print(int(a) is not int(b))
+print(type(a))
+
+#string
+firstName = input("enter a name")
+lastName = input("enter a name")
+fullName = firstName + " " + lastName
+print(fullName)
+print(fullName.isnumeric())
+print(fullName.isalnum())
+print(fullName.title())
+
+print(type(fullName))
+
+s = input("enter a value")
+t = input("enter a value")
+print(s + t)
+print(type(s + t))
+
+
+
+
+
